6b77d27b6d5536f8f750d95f7d5b92a74feaf72a
cbt26 <chxbht@gmail.com>
2026-04-15 21:50:51 +0200

    f

--- maps/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ maps/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"3.",p:["p1"]},{w:"Conseils",p:["p1"]},{w:"et",p:["p1","p2"]},{w:"astuces",p:["p1"]},{w:"2.",p:["p2"]},{w:"R\u00E9glage",p:["p2"]},{w:"inspection",p:["p2"]},{w:"1.",p:["p3"]},{w:"Objectif",p:["p3"]},{w:"de",p:["p3"]},{w:"la",p:["p3"]},{w:"documentation",p:["p3"]},{w:"Rapport",p:["p4"]},{w:"des",p:["p4"]},{w:"modifications",p:["p4"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"3.",p:["p1"]},{w:"Conseils",p:["p1"]},{w:"et",p:["p1","p2"]},{w:"astuces",p:["p1"]},{w:"2.",p:["p2"]},{w:"Composants",p:["p2"]},{w:"r\u00E9glages",p:["p2"]},{w:"1.",p:["p3"]},{w:"Moissonneuse-batteuse",p:["p3"]},{w:"s\u00E9rie",p:["p3"]},{w:"S",p:["p3"]},{w:"(tournesol)",p:["p3"]},{w:"Rapport",p:["p4"]},{w:"des",p:["p4"]},{w:"modifications",p:["p4"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6];
